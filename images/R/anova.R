fe6071f088a2649b1b5038e0f5b1df28e4d6b32b
egarpor <edugarpor@gmail.com>
2016-10-03 01:34:13 +0200

    Anova

---
+++ images/R/anova.R
@@ -0,0 +1,33 @@
+
+# Generate linear model
+set.seed(4567)
+n <- 25
+x <- rnorm(n, sd = 2)
+y <- -0.5 + 0.5 * x + rnorm(n)
+mod <- lm(y ~ x)
+my <- mean(y)
+
+png("anova1.png", width = 7, height = 7, res = 200, units = "in")
+plot(x, y, xlim = c(-4, 4), ylim = c(-4, 4), pch = 16, main = paste("SST =", round(sum((y - my)^2), 2)))
+abline(mod$coefficients, col = 2, lwd = 2)
+abline(h = my, col = 4, lty = 2)
+segments(x0 = x, y0 = my, x1 = x, y1 = y, col = 4)
+legend("topleft", legend = expression("Data", "Fitted line", "Sample mean " * bar(Y), (Y[i] - bar(Y))^2), lwd = 2, col = c(1, 2, 4, 4), lty = c(1, 1, 2, 1))
+dev.off()
+
+png("anova2.png", width = 7, height = 7, res = 200, units = "in")
+plot(x, y, xlim = c(-4, 4), ylim = c(-4, 4), pch = 16, main = paste("SSR =", round(sum((mod$fitted.values - my)^2), 2)))
+abline(mod$coefficients, col = 2, lwd = 2)
+abline(h = my, col = 3, lty = 2)
+segments(x0 = x, y0 = my, x1 = x, y1 = mod$fitted.values, col = 3)
+points(x, mod$fitted.values, pch = 16, col = 3)
+legend("topleft", legend = expression("Data", "Fitted line", "Sample mean " * bar(Y), (hat(Y)[i]- bar(Y))^2), lwd = 2, col = c(1, 2, 3, 3), lty = c(1, 1, 2, 1))
+dev.off()
+
+png("anova3.png", width = 7, height = 7, res = 200, units = "in")
+plot(x, y, xlim = c(-4, 4), ylim = c(-4, 4), pch = 16, main = paste("SSE =", round(sum((y - mod$fitted.values)^2), 2)))
+abline(mod$coefficients, col = 2, lwd = 2)
+segments(x0 = x, y0 = y, x1 = x, y1 = mod$fitted.values, col = 6)
+points(x, mod$fitted.values, pch = 16, col = 6)
+legend("topleft", legend = expression("Data", "Fitted line", (hat(Y)[i]- Y[i])^2), lwd = 2, col = c(1, 2, 6))
+dev.off()
