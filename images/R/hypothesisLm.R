a794bacc978655bb3efe40da94c2ed06f26f38f8
egarpor <edugarpor@gmail.com>
2016-10-04 11:10:08 +0200

    Improve some explanations

--- images/R/hypothesisLm.R
+++ images/R/hypothesisLm.R
@@ -59,51 +59,60 @@ png("linearmodelquiz.png", width = 10, height = 10, res = 200, units = "in")
 set.seed(223971)
 par(mfrow = c(3, 3), mar = c(4, 4, 2, 1) + 0.1)
 
+# OK
 x1 <- rexp(200)
 eps <- rnorm(200)
 y1 <- -0.5 * x1 + eps
 plot(x1, y1, pch = 16, xlab = "x", ylab = "y", main = "1")
 abline(lm(y1 ~ x1)$coefficients, col = 2, lwd = 2)
 
+# Nonlinear
 x2 <- runif(200)
 eps <- rnorm(200)
 y2 <- 1 - log(0.5 * x2) + eps
 plot(x2, y2, pch = 16, xlab = "x", ylab = "y", main = "2")
 abline(lm(y2 ~ x2)$coefficients, col = 2, lwd = 2)
 
+# Heteroskedastic
 x3 <- c(rpois(100, 5), rnorm(100))
 y3 <- 0.5 + 0.5 * x3 + 0.5 * sqrt(abs(x3)) * eps
 plot(x3, y3, pch = 16, xlab = "x", ylab = "y", main = "3")
 abline(lm(y3 ~ x3)$coefficients, col = 2, lwd = 2)
 
+# Non-normal
 x4 <- rnorm(200)
 epst <- rt(200, 2)
 y4 <- 0.5 * x4 + epst
 plot(x4, y4, pch = 16, xlab = "x", ylab = "y", main = "4")
 abline(lm(y4 ~ x4)$coefficients, col = 2, lwd = 2)
 
+# Dependent
 x5 <- rnorm(200)
 epsNoIndep <- sdetorus::rTrajOu(x0 = 0, alpha = 0.01, mu = 0, sigma = 0.5, N = 200 - 1, delta = 0.01)
 y5 <- 1 - 0.5 * x5 + epsNoIndep
 plot(x5, y5, pch = 16, xlab = "x", ylab = "y", main = "5")
 abline(lm(y5 ~ x5)$coefficients, col = 2, lwd = 2)
 
+# Non-normal
 x6 <- rnorm(200, sd = 0.25)
 eps <- rpois(200, 3) - 3
 y6 <- 1 + x6 + eps
 plot(x6, y6, pch = 16, xlab = "x", ylab = "y", main = "6")
 abline(lm(y6 ~ x6)$coefficients, col = 2, lwd = 2)
 
+# OK
 x7 <- rnbinom(200, 10, 0.5)
 y7 <- -0.1 * x7 + rnorm(100)
 plot(x7, y7, pch = 16, xlab = "x", ylab = "y", main = "7")
 abline(lm(y7 ~ x7)$coefficients, col = 2, lwd = 2)
 
+# Nonlinear, heteroskedastic
 x8 <- rnorm(200, sd = 2)
 y8 <- -1 + x8^3 + 5 * (x8 - min(x8)) * rnorm(200, sd = 2)
 plot(x8, y8, pch = 16, xlab = "x", ylab = "y", main = "8")
 abline(lm(y8 ~ x8)$coefficients, col = 2, lwd = 2)
 
+# Non linear, heteroskedastic
 x9 <- rt(200, 10)
 y9 <- -2 - 0.5 * log(abs(x9)) + rnorm(200)
 plot(x9, y9, pch = 16, xlab = "x", ylab = "y", main = "9")
