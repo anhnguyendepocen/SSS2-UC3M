4e21088a09240af0c628c338450efdb3ca25dfcd
egarpor <edugarpor@gmail.com>
2016-10-23 14:05:14 +0200

    Update visualizeFitLm with tick matching

--- images/R/visualizeFitLm.R
+++ images/R/visualizeFitLm.R
@@ -7,7 +7,7 @@
 #' @param data a \code{data.frame} containing \code{X} and \code{Y} as variables.
 #' @param nGrid number of cuts used to produce the XY grid.
 #' @param zTop upper z-limit of the 3D bounding box.
-#' @inheritParams base::persp
+#' @inheritParams graphics::persp
 #' @param alpha desired level for the theoretical confidence intervals.
 #' @return
 #' Nothing. The function is called to produce a plot.
@@ -94,8 +94,9 @@ visualizeFitLm <- function(data, nGrid = 6, zTop = 0.5, theta = -30, phi = 20,
 #' @description Generates a plot illustrating the key concepts for a fitted multiple linear model: linear trend, model-based confidence bands and normality around the mean.
 #'
 #' @param data a \code{data.frame} containing \code{X}, \code{Y} and \code{Z} as variables.
-#' @param nGrid number of cuts used to produce the XYZ grid.
+#' @param nGrid (approximate) number of cuts used to produce the XYZ grid. Computed to match the effective \code{nticks} in \code{\link[graphics]{persp}}.
 #' @inheritParams visualizeFitLm
+#' @inheritParams graphics::persp
 #' @param alpha desired level for the theoretical confidence intervals.
 #' @param basalScatter Add the projections of points in the XY plane?
 #' @return
@@ -104,15 +105,15 @@ visualizeFitLm <- function(data, nGrid = 6, zTop = 0.5, theta = -30, phi = 20,
 #' # Generate data
 #' X <- rnorm(100, sd = 1.5)
 #' Y <- rnorm(100, sd = 1.5)
-#' Z <- 0.5 - 0.25 * X + 0.25 * Y + rnorm(100, sd = 1.5)
+#' Z <- 0.5 - 0.25 * X + 0.15 * Y + rnorm(100)
 #' data <- data.frame(X = X, Y = Y, Z = Z)
 #'
 #' par(mar = rep(0, 4), oma = rep(0, 4))
 #' visualizeFitLm3D(data = data)
-#' visualizeFitLm3D(data = data, phi = 5)
+#' visualizeFitLm3D(data = data, phi = 10)
 #' @author Eduardo García-Portugués (\email{edgarcia@est-econ.uc3m.es}).
 #' @export
-visualizeFitLm3D <- function(data, nGrid = 6, theta = -30, phi = 20,
+visualizeFitLm3D <- function(data, nGrid = 5, theta = -30, phi = 20,
                              alpha = 0.05, basalScatter = TRUE) {
   
   # Estimate lm
@@ -128,9 +129,12 @@ visualizeFitLm3D <- function(data, nGrid = 6, theta = -30, phi = 20,
   gZ <- seq(min(data$Z) - sdZ, max(data$Z) + sdZ, length = nGrid)
   
   # Compute regression surface
-  lxy <- 21
-  x <- seq(gX[1], gX[nGrid], length = lxy)
-  y <- seq(gY[1], gY[nGrid], length = lxy)
+  x <- pretty(gX, nGrid)
+  y <- pretty(gY, nGrid)
+  x <- c(gX[1], x[x > gX[1] & x < gX[nGrid]], gX[nGrid])
+  y <- c(gY[1], y[y > gY[1] & y < gY[nGrid]], gY[nGrid])
+  lx <- length(x)
+  ly <- length(y)
   xy <- expand.grid(x = x, y = y)
   new <- data.frame(X = xy$x, Y = xy$y)
   pred <- predict(mod, newdata = new, type = "response")
@@ -139,31 +143,37 @@ visualizeFitLm3D <- function(data, nGrid = 6, theta = -30, phi = 20,
   sigma <- summary(mod)$sigma
   zDown <- qnorm(alpha/2, pred, sigma)
   zUp <- qnorm(1 - alpha/2, pred, sigma)
-
+  gZ <- seq(min(c(gZ, zDown)), max(c(gZ, zUp)), length = nGrid)
+  
   # Plot data and regression
   require(plot3D)
+  panelFirst <- function(pmat) {
+    
+    # Plot points projection in the basal plane
+    if (basalScatter) {
+      
+      XY <- trans3D(data$X, data$Y, rep(gZ[1], n), pmat = pmat)
+      scatter2D(XY$x, XY$y, pch = 16, cex = 0.5, add = TRUE, 
+                colkey = FALSE, col = 1)
+      
+    }
+    
+  }
   gridMat <- scatter3D(data$X, data$Y, data$Z, pch = 16, theta = theta, phi = phi, 
-                       bty = "g", nticks = nGrid, axes = FALSE, colkey = FALSE,
-                       col = 2, xlim = range(gX), ylim = range(gY), 
-                       zlim = range(c(gZ, zDown, zUp)),
-                       panel.first = function(pmat) {
-                         if (basalScatter) {
-                           XY <- trans3D(data$X, data$Y, rep(gZ[1], n), pmat = pmat)
-                           scatter2D(XY$x, XY$y, pch = 16, cex = 0.5, add = TRUE, 
-                                     colkey = FALSE, col = 1)
-                         }
-                       })
-  text(x = trans3d(median(gX), gY[1], gZ[1], gridMat), labels = "x", pos = 1)
-  text(x = trans3d(gX[1], median(gY), gZ[1], gridMat), labels = "y", pos = 2)
-  text(x = trans3d(gX[1], gY[nGrid], median(gZ), gridMat), labels = "z", pos = 2)
+                       bty = "g", axes = FALSE, colkey = FALSE, col = 2, 
+                       xlim = range(gX), ylim = range(gY), zlim = range(gZ), 
+                       panel.first = panelFirst, nticks = nGrid)
+  text(x = trans3d(median(gX), gY[1], gZ[1], gridMat), labels = "x1", pos = 1)
+  text(x = trans3d(gX[1], median(gY), gZ[1], gridMat), labels = "x2", pos = 2)
+  text(x = trans3d(gX[1], gY[nGrid], median(gZ), gridMat), labels = "y", pos = 2)
   
   # Plot confidence region
   M <- mesh(x, y)
-  surf3D(x = M$x, y = M$y, z = matrix(zUp, lxy, lxy), col = "yellow", 
-         alpha = 0.1, add = TRUE, NAcol = NULL)
-  surf3D(x = M$x, y = M$y, z = matrix(zDown, lxy, lxy), col = "yellow", 
-         alpha = 0.1, add = TRUE, NAcol = NULL)
-  surf3D(x = M$x, y = M$y, z = matrix(pred, lxy, lxy), col = "lightblue", 
-         alpha = 0.3, border = gray(0.5), add = TRUE, NAcol = NULL)
+  surf3D(x = M$x, y = M$y, z = matrix(zUp, nrow = lx, ncol = ly), col = "yellow", 
+         alpha = 0.1, add = TRUE, border = "yellow2")
+  surf3D(x = M$x, y = M$y, z = matrix(zDown, nrow = lx, ncol = ly), col = "yellow", 
+         alpha = 0.1, add = TRUE, border = "yellow2")
+  surf3D(x = M$x, y = M$y, z = matrix(pred, nrow = lx, ncol = ly), col = "lightblue", 
+         alpha = 0.3, border = gray(0.5), add = TRUE)
   
 }
