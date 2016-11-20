b5b297bb047ead1d3a4a316d1156a4ee79ed45f2
egarpor <edugarpor@gmail.com>
2016-11-20 18:38:44 +0100

    Reporting

--- reporting/report.R
+++ reporting/report.R
@@ -1,7 +1,6 @@
-# With this script we reproduce the multiple linear analysis of the Boston dataset
-
-# Not necessary if we are in R Commander
-library(RcmdrMisc)
+# A simple and non-exhaustive analysis for the price of the houses in the Boston
+# dataset. The purpose is to quantify, by means of a multiple linear model,
+# the effect of 14 variables in the price of a house in the suburbs of Boston.
 
 # Import data
 library(MASS)
@@ -11,24 +10,21 @@ data(Boston)
 mod <- lm(medv ~ ., data = Boston)
 summary(mod)
 
-# Check the diagnostic plots
-plot(mod, 1)
-
-# Seems like there is some non-linearity!
+# Check the linearty assumption
+plot(mod, 1) # Clear non-linearity
 
 # Let's consider the transformations given in Harrison and Rubinfeld (1978)
 modTransf <- lm(I(log(medv * 1000)) ~ I(rm^2) + age + log(dis) +
                   log(rad) + tax + ptratio + I(black / 1000) +
                   I(log(lstat / 100)) + crim + zn + indus + chas +
-                  I((10*nox)^2), data = Boston)
+                  I((10 * nox)^2), data = Boston)
 summary(modTransf)
 
-# Check the diagnostic plots
-plot(modTransf, 1)
 # The non-linearity is more subtle now
+plot(modTransf, 1)
 
 # Look for the best model in terms of the BIC
-modTransfBIC <- stepwise(modTransf)
+modTransfBIC <- stepwise(modTransf, trace = 0)
 summary(modTransfBIC)
 
 # Let's explore the most significant variables, to see if the model can be
@@ -36,11 +32,11 @@ summary(modTransfBIC)
 mod3D <- lm(I(log(medv * 1000)) ~ I(log(lstat / 100)) + crim, data = Boston)
 summary(mod3D)
 
-# With only 2 variables, we explain the 72% of variability. 
-# Compared with the 80% with 10 variables, it is an important improvement 
+# With only 2 variables, we explain the 72% of variability.
+# Compared with the 80% with 10 variables, it is an important improvement
 # in terms of simplicity.
 
-# Let's add these variables to the dataset, so we can call scatterplotMatrix 
+# Let's add these variables to the dataset, so we can call scatterplotMatrix
 # and scatter3d through R Commander's menu
 Boston$logMedv <- log(Boston$medv * 1000)
 Boston$logLstat <- log(Boston$lstat / 100)
